b0f6bbdbc2b4c7ee5b0b3a9d07fd51d249c337c1
mnwa2006 <96056124+mnwa2006@users.noreply.github.com>
2026-05-12 22:50:14 +0300

    Update RenderClothConfig.java
    add HEAT_HUD_ENABLE option

--- src/main/java/com/tacz/guns/compat/cloth/client/RenderClothConfig.java
+++ src/main/java/com/tacz/guns/compat/cloth/client/RenderClothConfig.java
@@ -52,6 +52,10 @@ public class RenderClothConfig {
                 .setDefaultValue(true).setTooltip(Component.translatable("config.tacz.client.render.gun_hud_enable.desc"))
                 .setSaveConsumer(RenderConfig.GUN_HUD_ENABLE::set).build());
 
+        render.addEntry(entryBuilder.startBooleanToggle(Component.translatable("config.tacz.client.render.heat_hud_enable"), RenderConfig.HEAT_HUD_ENABLE.get())
+                .setDefaultValue(true).setTooltip(Component.translatable("config.tacz.client.render.heat_hud_enable.desc"))
+                .setSaveConsumer(RenderConfig.HEAT_HUD_ENABLE::set).build());
+
         render.addEntry(entryBuilder.startBooleanToggle(Component.translatable("config.tacz.client.render.kill_amount_enable"), RenderConfig.KILL_AMOUNT_ENABLE.get())
                 .setDefaultValue(true).setTooltip(Component.translatable("config.tacz.client.render.kill_amount_enable.desc"))
                 .setSaveConsumer(RenderConfig.KILL_AMOUNT_ENABLE::set).build());
